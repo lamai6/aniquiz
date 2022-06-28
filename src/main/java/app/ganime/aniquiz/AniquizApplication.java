f9fd35ba360d66c4943d0f52ef45e7bb60582534
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-06-28 22:59:28 +0200

    chore: initial commit

---
+++ src/main/java/app/ganime/aniquiz/AniquizApplication.java
@@ -0,0 +1,13 @@
+package app.ganime.aniquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ganime/aniquiz/AniquizApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ganime.aniquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
