e2fc7397efc48c54bd9d637cd71f681cd0ee4afe
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-08-03 20:27:49 +0200

    test(series): implement cucumber feature step definitions

---
+++ src/test/java/app/ganime/aniquiz/it/series/SeriesStepDefinitions.java
@@ -0,0 +1,67 @@
+package app.ganime.aniquiz.it.series;
+
+import app.ganime.aniquiz.it.HttpClient;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import lombok.Data;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SeriesStepDefinitions {
+
+	@Autowired
+	private HttpClient httpClient;
+	@Autowired
+	private ObjectMapper mapper;
+
+	private String name;
+	private LocalDate releaseDate;
+	private JSONObject postBody;
+	private final String SERIES_URI = "series";
+	private final String SERIES_NAME_JSON_KEY = "name";
+	private final String SERIES_RELEASE_DATE_JSON_KEY = "release_date";
+
+	@Given("the series name is {string}")
+	public void the_series_is(String name) {
+		this.name = name;
+	}
+
+	@And("its release date is {int}-{int}-{int}")
+	public void the_series_release_date_is(int year, int month, int day) {
+		this.releaseDate = LocalDate.of(year, month, day);
+	}
+
+	@When("the user sends the series")
+	public void the_user_sends_the_series() throws JSONException {
+		JSONObject series = new JSONObject();
+		series.put(SERIES_NAME_JSON_KEY, name);
+		series.put(SERIES_RELEASE_DATE_JSON_KEY, releaseDate);
+
+		ResponseEntity<String> response = this.httpClient.post(SERIES_URI, series.toString(), String.class);
+		this.postBody = new JSONObject(response.getBody());
+	}
+
+	@Then("the series is added")
+	public void the_series_is_added() throws JSONException {
+		int resourceId = this.postBody.getInt("id");
+		SeriesDTO series = (SeriesDTO) this.httpClient.get(SERIES_URI, resourceId, SeriesDTO.class).getBody();
+
+		assertThat(series.getName()).isEqualTo(this.name);
+		assertThat(series.getReleaseDate()).isEqualTo(this.releaseDate);
+	}
+}
--- src/test/resources/features/series.feature
+++ src/test/resources/features/series.feature
@@ -3,5 +3,5 @@ Feature: Series
   Scenario: Adding a new series
     Given the series name is "One Piece"
     And its release date is 1999-10-20
-    When the user send the series
-    Then the series should be added
+    When the user sends the series
+    Then the series is added
