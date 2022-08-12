7e40d80500b24fc0537b98c5df952191f16ed38f
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-08-12 23:21:57 +0200

    feat(series): implement dto

--- src/main/java/app/ganime/aniquiz/series/SeriesDTO.java
+++ src/main/java/app/ganime/aniquiz/series/SeriesDTO.java
@@ -1,14 +1,27 @@
 package app.ganime.aniquiz.series;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@Jacksonized
 public class SeriesDTO {
+
+	@JsonProperty("name")
 	private String name;
+
+	@JsonProperty("author")
 	private String author;
+
+	@JsonProperty("release_date")
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	private LocalDate releaseDate;
 }
