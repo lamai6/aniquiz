46bc1f1de95e1253f69cbb65db53efff7fb7ff4d
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-08-31 22:02:46 +0200

    chore(test): configure h2 database to run repository unit tests

--- src/test/java/app/ganime/aniquiz/ut/series/SeriesRepositoryUnitTest.java
+++ src/test/java/app/ganime/aniquiz/ut/series/SeriesRepositoryUnitTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,20 +19,23 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ExtendWith(MockitoExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:aniquiz_ut;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL"
+})
 public class SeriesRepositoryUnitTest {
 
 	@Autowired
 	private SeriesRepository repository;
 
-	private List<Series> databaseSeries;
+	private static List<Series> databaseSeries;
 
 	@BeforeAll
-	public void initData() {
+	public static void initData() {
 		databaseSeries = Stream.of(
-				new Series(1,"One Piece","Eichiro Oda", LocalDate.of(1999, 10, 20)),
-				new Series(2,"Bleach","Tite Kubo", LocalDate.of(2001, 8, 7)),
-				new Series(3,"Hajime no Ippo","Joji Morikawa", LocalDate.of(1989, 10, 11)))
+				new Series(1L,"One Piece","Eichiro Oda", LocalDate.of(1999, 10, 20)),
+				new Series(2L,"Bleach","Tite Kubo", LocalDate.of(2001, 8, 7)),
+				new Series(3L,"Hajime no Ippo","Joji Morikawa", LocalDate.of(1989, 10, 11)))
 			.collect(Collectors.toList());
 	}
 
@@ -66,7 +69,7 @@ public class SeriesRepositoryUnitTest {
 
 	@Test
 	public void should_add_series() {
-		Series series = new Series(4,"Hunter x Hunter", "Yoshihiro Togashi", LocalDate.of(1998, 3, 16));
+		Series series = new Series(4L,"Hunter x Hunter", "Yoshihiro Togashi", LocalDate.of(1998, 3, 16));
 
 		Series seriesAdded = repository.save(series);
 
