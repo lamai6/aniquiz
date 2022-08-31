f59cec12bdd18185332c1df9a73130eb1a43d2c9
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-08-31 21:59:05 +0200

    feat(series): implement repository

--- src/main/java/app/ganime/aniquiz/series/Series.java
+++ src/main/java/app/ganime/aniquiz/series/Series.java
@@ -1,15 +1,29 @@
 package app.ganime.aniquiz.series;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "series")
 public class Series {
-	private int id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
 	private String name;
+
+	@Column(nullable = false)
 	private String author;
+
+	@Column(nullable = false)
 	private LocalDate releaseDate;
 }
--- src/main/java/app/ganime/aniquiz/series/SeriesRepository.java
+++ src/main/java/app/ganime/aniquiz/series/SeriesRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface SeriesRepository extends PagingAndSortingRepository<Series, Long> {
+	@Override
 	List<Series> findAll();
 }
