a308e797ec55813177466372161b78054f6157b3
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-09-05 11:52:40 +0200

    fix(series): fix 'no suitable resolver' exception when exception handler is triggered

--- src/main/java/app/ganime/aniquiz/config/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/app/ganime/aniquiz/config/exception/RestResponseEntityExceptionHandler.java
@@ -2,16 +2,16 @@ package app.ganime.aniquiz.config.exception;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	protected ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex, HttpHeaders headers, WebRequest request) {
-		return handleExceptionInternal(ex, ex.getMessage(), headers, ex.getStatus(), request);
+	protected ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request) {
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), ex.getStatus(), request);
 	}
 }
