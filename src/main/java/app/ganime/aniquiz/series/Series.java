7b28ec3cdc4b9dfa78e2361db1219dc3d48c6420
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-09-21 12:52:22 +0200

    fix(series): fix entity id generation strategy

--- src/main/java/app/ganime/aniquiz/series/Series.java
+++ src/main/java/app/ganime/aniquiz/series/Series.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class Series {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
