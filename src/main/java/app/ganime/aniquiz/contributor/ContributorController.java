bd65f1266427583aeddec289bb787a922abb74f1
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-10-17 16:34:32 +0200

    feat(contributor): implement controller

--- src/main/java/app/ganime/aniquiz/contributor/ContributorController.java
+++ src/main/java/app/ganime/aniquiz/contributor/ContributorController.java
@@ -1,9 +1,40 @@
 package app.ganime.aniquiz.contributor;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/contributors")
 public class ContributorController {
 
+	@Autowired
 	private ContributorService service;
+	@Autowired
 	private ModelMapper mapper;
+
+	@GetMapping
+	public List<ContributorDTO> getContributors() {
+		List<Contributor> contributors = service.getContributors();
+		return contributors.stream()
+			.map(c -> mapper.map(c, ContributorDTO.class))
+			.collect(Collectors.toList());
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Long postContributor(@RequestBody ContributorDTO contributorDTO) {
+		Contributor contributor = mapper.map(contributorDTO, Contributor.class);
+		return service.saveContributor(contributor).getId();
+	}
+
+	@GetMapping("/{id}")
+	public ContributorDTO getContributor(@PathVariable("id") Long id) {
+		Contributor contributor = service.getContributor(id);
+		return mapper.map(contributor, ContributorDTO.class);
+	}
 }
