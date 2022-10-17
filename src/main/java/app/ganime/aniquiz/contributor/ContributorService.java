71abca0000881d2518b49de799e456f2accd5906
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-10-17 22:41:38 +0200

    feat(contributor): implement service

--- src/main/java/app/ganime/aniquiz/contributor/ContributorService.java
+++ src/main/java/app/ganime/aniquiz/contributor/ContributorService.java
@@ -1,18 +1,26 @@
 package app.ganime.aniquiz.contributor;
 
+import app.ganime.aniquiz.config.error.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class ContributorService {
 
+	@Autowired
+	public ContributorRepository repository;
+
 	public Contributor getContributor(Long id) {
-		return null;
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException());
 	}
 
 	public List<Contributor> getContributors() {
-		return null;
+		return repository.findAll();
 	}
 
 	public Contributor saveContributor(Contributor contributor) {
-		return null;
+		return repository.save(contributor);
 	}
 }
