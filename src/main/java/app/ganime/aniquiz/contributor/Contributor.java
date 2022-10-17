321f7e0c853e1357fac7e7f8b45daaef09cc10ce
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-10-17 22:57:39 +0200

    feat(contributor): implement repository

--- src/main/java/app/ganime/aniquiz/contributor/Contributor.java
+++ src/main/java/app/ganime/aniquiz/contributor/Contributor.java
@@ -2,15 +2,29 @@ package app.ganime.aniquiz.contributor;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "author")
 public class Contributor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
 	private String username;
+
+	@Column(nullable = false)
 	private String email;
+
+	@Column(nullable = false)
 	private String password;
 }
