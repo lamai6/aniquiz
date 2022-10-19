3920a86428dfeb6d0263568466a132e58c2a485c
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-10-19 16:07:26 +0200

    test(contributor): clean test database after each scenario and fix some step definitions

--- src/test/java/app/ganime/aniquiz/it/contributor/ContributorStepDefinitions.java
+++ src/test/java/app/ganime/aniquiz/it/contributor/ContributorStepDefinitions.java
@@ -2,36 +2,53 @@ package app.ganime.aniquiz.it.contributor;
 
 import app.ganime.aniquiz.config.error.ApiErrorDTO;
 import app.ganime.aniquiz.contributor.ContributorDTO;
+import app.ganime.aniquiz.contributor.ContributorRepository;
 import app.ganime.aniquiz.it.HttpClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 public class ContributorStepDefinitions {
 
 	@Autowired
 	private HttpClient httpClient;
+	@Autowired
+	private ContributorRepository repository;
 
+	private ObjectMapper mapper;
 	private String username;
 	private String email;
 	private String password;
-	private JSONObject responseBody;
-	private final String CONTRIBUTOR_URI = "contributor";
+	private String body;
+	private final String CONTRIBUTOR_URI = "contributors";
 	private final String USERNAME_JSON_KEY = "username";
 	private final String EMAIL_JSON_KEY = "email";
 	private final String PASSWORD_JSON_KEY = "password";
 	private final String ID_KEY = "id";
 	private final String ERROR_KEY = "error";
-	private final String ERROR_MSG = "The account creation failed";
+	private final String ERROR_MSG = "The account registration failed";
 	private final String INVALID_EMAIL = "Invalid email";
 
-	@Given("the contributor name is {string}")
+	public ContributorStepDefinitions() {
+		this.mapper = new ObjectMapper();
+	}
+
+	@After
+	public void cleanDatabase() {
+		repository.deleteAll();
+	}
+
+	@Given("the contributor username is {string}")
 	public void the_contributor_name_is(String username) {
 		this.username = username;
 	}
@@ -53,25 +70,25 @@ public class ContributorStepDefinitions {
 		contributor.put(EMAIL_JSON_KEY, email);
 		contributor.put(PASSWORD_JSON_KEY, password);
 
-		String body = (String) this.httpClient.post(CONTRIBUTOR_URI, contributor.toString(), String.class).getBody();
-		this.responseBody = new JSONObject(body);
+		this.body = (String) this.httpClient.post(CONTRIBUTOR_URI, contributor.toString(), String.class).getBody();
 	}
 
 	@Then("the contributor is registered")
 	public void the_contributor_is_registered() throws JSONException {
-		ContributorDTO contributor = (ContributorDTO) this.httpClient.get(CONTRIBUTOR_URI, responseBody.getInt(ID_KEY), ContributorDTO.class).getBody();
+		ContributorDTO contributor = (ContributorDTO) this.httpClient.get(CONTRIBUTOR_URI, Integer.parseInt(body), ContributorDTO.class).getBody();
 
 		assertThat(contributor.getUsername()).isEqualTo(this.username);
-		assertThat(contributor.getEmail()).isEqualTo(this.username);
+		assertThat(contributor.getEmail()).isEqualTo(this.email);
 	}
 
 	@Then("the registration is rejected")
-	public void the_registration_is_rejected() throws JSONException {
-		ApiErrorDTO.ErrorBlock error = (ApiErrorDTO.ErrorBlock) responseBody.get(ERROR_KEY);
-		ApiErrorDTO.Error emailError = error.getErrors().stream().findFirst().orElse(null);
+	public void the_registration_is_rejected() throws Exception {
+		ApiErrorDTO error = mapper.readValue(body, ApiErrorDTO.class);
+		ApiErrorDTO.Error emailError = error.getError().getErrors().stream().findFirst().orElse(null);
+		log.info(error.toString());
 
-		assertThat(error.getCode()).isEqualTo(400);
-		assertThat(error.getMessage()).isEqualTo(ERROR_MSG);
+		assertThat(error.getError().getCode()).isEqualTo(400);
+		assertThat(error.getError().getMessage()).isEqualTo(ERROR_MSG);
 		assertThat(emailError.getMessage()).isEqualTo(INVALID_EMAIL);
 	}
 }
