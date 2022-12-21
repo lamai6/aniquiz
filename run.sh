1084f5289dc5a980945d3a55028109b356e7681a
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-12-21 23:19:49 +0100

    chore(docker): fix compile script to detect changes in src/main only

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ chmod +x mvnw
 dos2unix mvnw
 
 ./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
-while inotifywait -r -e CLOSE_WRITE,MODIFY --format '%:e %f' "src/"
+while inotifywait -r -e CLOSE_WRITE,MODIFY --format '%:e %f' "src/main/"
 do
   ./mvnw compile
 done
