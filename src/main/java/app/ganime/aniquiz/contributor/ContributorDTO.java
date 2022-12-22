ab6b4bbc92960852be776ddb2abeb60cfebfbe41
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-12-22 14:08:51 +0100

    fix(contributor): hide password when serializing dto

--- src/main/java/app/ganime/aniquiz/contributor/ContributorDTO.java
+++ src/main/java/app/ganime/aniquiz/contributor/ContributorDTO.java
@@ -1,6 +1,7 @@
 package app.ganime.aniquiz.contributor;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ public class ContributorDTO {
 	private String username;
 	@Email(message = "{contributor.email.invalid}")
 	private String email;
+	@JsonIgnore
 	private String password;
 	private String roles;
 	@JsonProperty("created_at")
